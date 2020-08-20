36f71184d9192866b2077c903cc68585df4b37ee
AlexDays <AlexDays@students.softuni.bg>
2020-08-20 15:44:08 +0300

    Add "PointInRectangle" project

--- WorkingWithAbstraction/PointInRectangle/Program.cs
+++ WorkingWithAbstraction/PointInRectangle/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
+using System.Linq;
 namespace PointInRectangle
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var coordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+			var rectangle = new Rectangle(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+			var n = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < n; i++)
+			{
+				var currerntCoordinates = Console.ReadLine().Split().Select(int.Parse).ToArray();
+				var point = new Point(currerntCoordinates[0], currerntCoordinates[1]);
+				var isInside = rectangle.Contains(point);
+				Console.WriteLine(isInside);
+			}
 		}
 	}
 }
---
+++ WorkingWithAbstraction/PointInRectangle/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿namespace PointInRectangle
+{
+	public class Rectangle
+	{
+		public Rectangle(int topLeftX, int topLeftY, int bottomRightX, int bottomRightY)
+		{
+			TopLeft = new Point(topLeftX, topLeftY);
+			BottomRight = new Point(bottomRightX, bottomRightY);
+		}
+
+		public Point TopLeft { get; set; }
+
+		public Point BottomRight { get; set; }
+
+		public bool Contains(Point point)
+		{
+			var XIsInside = (this.TopLeft.X <= point.X) && (point.X <= this.BottomRight.X);
+			var YIsInside = (this.TopLeft.Y <= point.Y) && (point.Y <= this.BottomRight.Y);
+
+			return XIsInside && YIsInside;
+		}
+	}
+}
