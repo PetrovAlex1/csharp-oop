537bd034cfde4f7c8ac3c74141ee43d2b4bab4a7
AlexDays <AlexDays@students.softuni.bg>
2020-09-03 15:16:04 +0300

    Add "Salary" project

---
+++ Encapsulation/Salary/Person.cs
@@ -0,0 +1,42 @@
+﻿namespace PersonsInfo
+{
+	public class Person
+	{
+		public Person(string firstName, string lastName, int age, decimal salary)
+		{
+			this.FirstName = firstName;
+			this.LastName = lastName;
+			this.Age = age;
+			this.Salary = salary;
+		}
+
+		public string FirstName { get; private set; }
+
+		public string LastName { get; private set; }
+
+		public int Age { get; private set; }
+
+		public decimal Salary { get; private set; }
+
+		public override string ToString()
+		{
+			return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+		}
+
+		public void IncreaseSalary(decimal percentage)
+		{
+			var increse = 0;
+
+			if (this.Age > 30)
+			{
+				increse = 100;
+			}
+			else
+			{
+				increse = 200;
+			}
+
+			Salary += Salary * (percentage / increse);
+		}
+	}
+}
--- Encapsulation/Salary/Program.cs
+++ Encapsulation/Salary/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Salary
+namespace PersonsInfo
 {
-	class Program
+	public class StartUp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var lines = int.Parse(Console.ReadLine());
+			var persons = new List<Person>();
+
+			for (int i = 0; i < lines; i++)
+			{
+				var input = Console.ReadLine().Split();
+				var person = new Person(input[0],
+										input[1],
+										int.Parse(input[2]),
+										decimal.Parse(input[3]));
+
+				persons.Add(person);
+			}
+
+			var parcentage = decimal.Parse(Console.ReadLine());
+			persons.ForEach(p => p.IncreaseSalary(parcentage));
+			persons.ForEach(p => Console.WriteLine(p.ToString()));
 		}
 	}
 }
