d09631a6298ef0cebb9aea1a16d3dce8ca50af78
AlexDays <AlexDays@students.softuni.bg>
2020-09-03 14:54:01 +0300

    Add "Persons" project

--- Encapsulation/Persons/Program.cs
+++ Encapsulation/Persons/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Persons
+namespace PersonsInfo
 {
-	class Program
+	public class StartUp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			var lines = int.Parse(Console.ReadLine());
+			var persons = new List<Person>();
+
+			for (int i = 0; i < lines; i++)
+			{
+				var input = Console.ReadLine().Split().ToArray();
+				var person = new Person(input[0], input[1], int.Parse(input[2]));
+				persons.Add(person);
+			}
+
+			persons.OrderBy(p => p.FirstName)
+				   .ThenBy(p => p.Age)
+				   .ToList()
+				   .ForEach(p => Console.WriteLine(p.ToString()));
+
 		}
 	}
 }
