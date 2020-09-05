143175c000f4da9935693503e9a02ee26cf976f4
AlexDays <AlexDays@students.softuni.bg>
2020-09-05 19:16:00 +0300

    Add "AnimalFarm" project

--- EncapsulationExercise/AnimalFarm/Program.cs
+++ EncapsulationExercise/AnimalFarm/Program.cs
@@ -6,7 +6,15 @@ namespace AnimalFarm
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string name = Console.ReadLine();
+			int age = int.Parse(Console.ReadLine());
+
+			var chicken = new Chicken(name, age);
+			Console.WriteLine(
+				"Chicken {0} (age {1}) can produce {2} eggs per day.",
+				chicken.Name,
+				chicken.Age,
+				chicken.ProductPerDay);
 		}
 	}
 }
