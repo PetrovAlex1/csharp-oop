439a68ac5e3049f235a9546e26fa17cf2953ace1
AlexDays <AlexDays@students.softuni.bg>
2020-09-17 14:16:22 +0300

    Add "Cars" project

---
+++ InterfacesAndAbstraction/Cars/Seat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Cars
+{
+	public class Seat : ICar
+	{
+		public string Model { get; private set; }
+
+		public string Color { get; private set; }
+
+		public string Start()
+		{
+			return "Engine start";
+		}
+
+		public string Stop()
+		{
+			return "Breaaak!";
+		}
+
+		public Seat(string model, string color)
+		{
+			this.Model = model;
+			this.Color = color;
+		}
+
+		public override string ToString()
+		{
+			var sb = new StringBuilder();
+			sb.AppendLine($"{this.Color} Seat {this.Model}");
+			sb.AppendLine(this.Start());
+			sb.AppendLine(this.Stop());
+
+			return sb.ToString();
+		}
+	}
+}
