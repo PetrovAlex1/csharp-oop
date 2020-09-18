16f0bed179b694a20407e629a009de5e5c03ba09
AlexDays <AlexDays@students.softuni.bg>
2020-09-18 10:47:43 +0300

    Add "DefineAnInterfaceIPerson" project

--- InterfacesAndAbstractionExercise/DefineAnInterfaceIPerson/Program.cs
+++ InterfacesAndAbstractionExercise/DefineAnInterfaceIPerson/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace DefineAnInterfaceIPerson
+namespace PersonInfo
 {
-	class Program
+	public class StartUp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string name = Console.ReadLine();
+			int age = int.Parse(Console.ReadLine());
+			IPerson person = new Citizen(name, age);
+			Console.WriteLine(person.Name);
+			Console.WriteLine(person.Age);
 		}
 	}
 }
