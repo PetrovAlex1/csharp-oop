0253f7fb9b5fe3dd2796e13d1a568a7bd957d651
AlexDays <AlexDays@students.softuni.bg>
2020-09-18 11:10:44 +0300

    Add "MultipleImplementation" project

--- InterfacesAndAbstractionExercise/MultipleImplementation/Program.cs
+++ InterfacesAndAbstractionExercise/MultipleImplementation/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace MultipleImplementation
+namespace PersonInfo
 {
-	class Program
+	public class StartUp
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			string name = Console.ReadLine();
+			int age = int.Parse(Console.ReadLine());
+			string id = Console.ReadLine();
+			string birthdate = Console.ReadLine();
+			IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
+			IBirthable birthable = new Citizen(name, age, id, birthdate);
+			Console.WriteLine(identifiable.Id);
+			Console.WriteLine(birthable.Birthdate);
+
 		}
 	}
 }
